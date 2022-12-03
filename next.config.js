83c1c2ee27786566b7dae02f46ecabffb04a9bf2
JanjyTapYT <81481526+JanjyTapYT@users.noreply.github.com>
2022-12-03 11:21:31 +0100

    updated to JavaScript

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  images: {
+    domains: [ "cdn.discordapp.com", "i.imgur.com", "localhost:3000" ]
+},
+/*
+async redirects() {
+  return [
+      {
+        source: '/link',
+        destination: 'https://link_href.com',
+        permanent: true, // true ? false
+      },
+    ]
+  }
+*/
+});
